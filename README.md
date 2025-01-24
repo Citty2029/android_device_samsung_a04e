a0a53efc0b3e4b569697cf0cb4b65996b87eeee1
Samsung-Galaxy-G85-JDM <sathnidu.2022597@ninaro.lk>
2025-01-24 11:56:23 +0000

    a05m: adpted to common tree

--- README.md
+++
@@ -1,9 +0,0 @@
-# Android device tree for samsung SM-A055F (a05m)
-
-```
-#
-# Copyright (C) 2024 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-```
--- lineage_a05m.mk
+++ lineage_a05m.mk
@@ -15,7 +15,9 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/a05m/device.mk)
 
 # Inherit from wingcamera-samsung
-#$(call inherit-product, vendor/samsung/wing-camera/wingcamera-samsung.mk)
+$(call inherit-product, vendor/samsung/wing-camera/wingcamera-samsung.mk)
+
+TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_DEVICE := a05m
 PRODUCT_NAME := lineage_a05m
@@ -24,12 +26,9 @@ PRODUCT_MODEL := Galaxy A05
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
-#PRODUCT_NO_CAMERA := true
 
-#PRODUCT_BUILD_PROP_OVERRIDES += \
-#    PRIVATE_BUILD_DESC="a05mxx-user 14 UP1A.231005.007 A055FXXS6CXI2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="a05mxx-user 14 UP1A.231005.007 A055FXXS7CXJ1 release-keys" \
+    BuildFingerprint=samsung/a05mxx/a05m:14/UP1A.231005.007/A055FXXS7CXJ1:user/release-keys
 
-#BUILD_FINGERPRINT := samsung/a05mxx/a05m:14/UP1A.231005.007/A055FXXS6CXI2:user/release-keys
 
-#PRODUCT_SYSTEM_PROPERTY_OVERRIDES += \
-#     ro.debuggable=0
