50a12ad9d17d1268d6b32af5a11439b2cab01b6d
Smiley <59495159+smiley9000@users.noreply.github.com>
2025-02-04 09:37:18 +0530

    a04e: bringup a04e

--- lineage_a04e.mk
+++ lineage_a04e.mk
@@ -12,23 +12,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from a05m device
-$(call inherit-product, device/samsung/a05m/device.mk)
+$(call inherit-product, device/samsung/a04e/device.mk)
 
 # Inherit from hq-camera-samsung
 $(call inherit-product, vendor/samsung/hq-camera/hqcamera-samsung.mk)
 
 TARGET_EXCLUDES_AUDIOFX := true
 
-PRODUCT_DEVICE := a05m
-PRODUCT_NAME := lineage_a05m
+PRODUCT_DEVICE := a04e
+PRODUCT_NAME := lineage_a04e
 PRODUCT_BRAND := Samsung
-PRODUCT_MODEL := Galaxy A05
+PRODUCT_MODEL := Galaxy A04e
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="a05mxx-user 14 UP1A.231005.007 A055FXXS7CXJ1 release-keys" \
-    BuildFingerprint=samsung/a05mxx/a05m:14/UP1A.231005.007/A055FXXS7CXJ1:user/release-keys
+    BuildDesc="a04exx-user 12 SP1A.210812.016 A042FXXSAEXJ3 release-keys" \
+    BuildFingerprint=samsung/a04exx/a04e:12/SP1A.210812.016/A042FXXSAEXJ3:user/release-keys
 
 
